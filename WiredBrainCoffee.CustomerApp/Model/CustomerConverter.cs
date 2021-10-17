86dbc267b758ae742a882be83979668fad3fff6a
Adebayo Salami <salamibolarinwa16@gmail.com>
2021-10-17 22:07:35 +0100

    vh

---
+++ WiredBrainCoffee.CustomerApp/Model/CustomerConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiredBrainCoffee.CustomerApp.Model
+{
+    public static class CustomerConverter
+    {
+        public static Customer CreateCutomerFromString(string inputString)
+        {
+            var values = inputString.Split(',');
+            return new Customer
+            {
+                Firstname = values[0],
+                Lastname = values[1],
+                IsDeveloper = bool.Parse(values[2])
+            };
+        }
+    }
+}
